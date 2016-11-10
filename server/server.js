8bcef3e5b1de3a86fe0e3b93932ab52f33b3ef4d
Peter <qnguyen523@gmail.com>
2016-11-09 20:55:50 -0800

    merge

--- server/server.js
+++ server/server.js
@@ -5,23 +5,23 @@ var _ = require('lodash');
 
 // front end from client folder
 app.use(express.static('client'));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 var lions = [];
 var id = 0;
 
 // implement back-end right here
-app.get('/lions', function (req, res) {
+app.get('/lions', function(req, res){
   res.json(lions);
 });
 
-app.get('/lions/:id', function (req, res) {
-  var lion = _.find(lions, { id: req.params.id });
+app.get('/lions/:id', function(req, res){
+  var lion = _.find(lions, {id: req.params.id});
   res.json(lion || {});
 });
 
-app.post('/lions', function (req, res) {
+app.post('/lions', function(req, res) {
   var lion = req.body;
   console.log(lion);
   id++;
@@ -30,18 +30,20 @@ app.post('/lions', function (req, res) {
   lions.push(lion);
 
   res.json(lion);
+  console.log("succesfully insert lion into lions array");
 });
 
 
-app.put('/lions/:id', function (req, res) {
+app.put('/lions/:id', function(req, res) {
   var update = req.body;
-  if (update.id) {
+  if (update.id)
+  {
     delete update.id
   }
   // console.log(update)
   console.log(update.id)
 
-  var lion = _.findIndex(lions, { id: req.params.id });
+  var lion = _.findIndex(lions, {id: req.params.id});
   if (!lions[lion]) {
     res.send();
   } else {
@@ -50,23 +52,19 @@ app.put('/lions/:id', function (req, res) {
   }
 });
 
-app.delete('/lions/:id', function (req, res) {
-  //  var deleteElement = req.body;
-  var lion = _.findIndex(lions, { id: req.params.id });
-  if (!lions[lion]) {
-    res.send("Error");
-    console.log("Error");
-  }
-  else {
-    console.log(lion);
-    console.log(lions[lion]);
-    lions.splice(lion, 1);
-  }
+app.delete('/lions/:id', function(req, res)
+{
+   var lion = _.findIndex(lions, {id: req.params.id});
+   console.log(lion);
+   console.log(lions[lion]);
+   lions.splice(lion,1);
+   res.json(req.lion);
 });
 
 app.set('port', (process.env.PORT || 8080));
 
-app.listen(app.get('port'), function () {
-  console.log("Node app is running at localhost:" + app.get('port'))
+app.listen(app.get('port'), function() {
+ console.log("Node app is running at localhost:" + app.get('port'));
+ console.log("Testing");
 });
 
